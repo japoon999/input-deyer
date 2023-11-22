93e38898f82c950a031ea4778b9d8234ee5372eb
japoon999 <aziz.azizli.95@inbox.ru>
2023-11-22 12:30:54 +0400

    first commit

---
+++ ap.js
@@ -0,0 +1,19 @@
+// let input= document.getElementById("input");
+// input.addEventListener ('click', function (e) {
+//     console.log(e.target.value);
+//     let input= document.getElementById("input").value;
+//     alert(input || "deyer yoxdur");
+// });
+
+
+
+// document.getElementById('btn').addEventListener('click', function () {
+//     var input = document.getElementById('input').value;
+//     alert(input || 'Dəyər daxil edilməyib');
+// });
+
+
+document.getElementById('btn').addEventListener('click', function () {
+    var inputValue = document.getElementById('inputValue').value;
+    alert(inputValue);
+});
